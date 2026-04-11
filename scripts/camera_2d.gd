2e2b0a32f5fdefb16ff540ca5f4915b524185897
BillIsAwesome1234 <thebenjiest@icloud.com>
2026-04-11 12:04:09 -0500

    camera again
    cool game better now

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -2,13 +2,23 @@ extends Camera2D
 
 @export var max_offset := 100.0
 @export var smoothing := 5.0
+@export var deadzone_radius := 50.0
 
 func _process(delta):
 	var screen_center = get_viewport_rect().size * 0.5
 	var mouse_position = get_viewport().get_mouse_position()
-	var offset = mouse_position - screen_center
+	var raw_offset = mouse_position - screen_center
 	
-	# Clamp how far the camera can move
+	var offset = Vector2.ZERO
+	
+	# Only move camera if outside deadzone
+	if raw_offset.length() > deadzone_radius:
+		# Remove the deadzone portion so movement starts smoothly
+		var direction = raw_offset.normalized()
+		var adjusted_length = raw_offset.length() - deadzone_radius
+		offset = direction * adjusted_length
+	
+	# Clamp to max distance
 	offset = offset.limit_length(max_offset)
 	
 	# Smooth movement
