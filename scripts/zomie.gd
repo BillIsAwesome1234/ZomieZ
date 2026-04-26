59669287351f13992ac9d0a9340aec207dcaf531
BillIsAwesome1234 <thebenjiest@icloud.com>
2026-04-26 14:03:27 -0500

    zomiiiiiiiiiiiiiie
    this game will be a game

---
+++ scripts/zomie.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+const SPEED = 50
+
+var direction := 0
+var state := "idle"
+var state_timer := 0.0
+
+func _ready():
+	randomize()
+	_pick_idle()
+
+func _physics_process(delta: float) -> void:
+	# Gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	state_timer -= delta
+
+	if state == "idle":
+		move(0)
+		if state_timer <= 0:
+			_pick_walk()
+
+	elif state == "walk":
+		move(direction)
+#TODO make zomie face correct way
+		if state_timer <= 0:
+			_pick_idle()
+
+	move_and_slide()
+
+func move(dir: int):
+	if dir != 0:
+		velocity.x = dir * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+# --- State helpers ---
+
+func _pick_idle():
+	state = "idle"
+	state_timer = randf_range(5.0, 10.0)
+
+func _pick_walk():
+	state = "walk"
+	direction = [-1, 1].pick_random()
+	state_timer = randf_range(1.5, 3.0) # how long it walks
