ddd921960e64c0134383a1b28ad5726c0ddfdca3
BillIsAwesome1234 <thebenjiest@icloud.com>
2026-04-26 13:33:53 -0500

    I think i made something?

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,11 +1,12 @@
 extends AnimatedSprite2D
 
+@onready var player: CharacterBody2D = $".."
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var mouse_pos = get_global_mouse_position()
+	look_at(mouse_pos)
+
+	if player.global_position.x > mouse_pos.x:
+		scale.y = -1
+	else:
+		scale.y = 1
